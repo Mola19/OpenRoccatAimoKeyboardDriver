b0b4f45cd9aee481cc846f2d275ba6245a55b391
Mola19 <Mola9041@gmail.com>
2025-10-26 03:08:00 +0100

    add getDeviceInfo for TKL Pro and 100

--- AimoKeyboardDriver.hpp
+++ AimoKeyboardDriver.hpp
@@ -2,16 +2,16 @@
 
 #include <hidapi.h>
 
+#include <cstdint>
+#include <expected>
 #include <map>
 #include <string>
 
 #include "ReadCallback.hpp"
 
-#define ROCCAT_VULCAN_TKL_PRO_PID 0x311A
 
-struct AimoKeyboardConfig {
-	uint8_t protocol_version;
-};
+#define ROCCAT_VULCAN_100_AIMO_PID 0x307A
+#define ROCCAT_VULCAN_TKL_PRO_PID 0x311A
 
 class AimoKeyboardDriver {
   public:
@@ -21,11 +21,45 @@ class AimoKeyboardDriver {
 		uint8_t blue;
 	};
 
+	struct Config {
+		uint8_t protocol_version;
+	};
+
+	enum PhysicalLayout {
+		ANSI = 0,
+		ISO = 1
+	};
+
+	// this list only contains layouts known layouts
+	// Japan and Korea exist based on their website, but the id is unknown
+	enum VisualLayout {
+		US = 0, // United States (ANSI)
+		DE = 1, // Germany (ISO)
+		UK = 2, // United Kingdom (ISO)
+		FR = 3, // France (ISO)
+	};
+
+	struct DeviceInfo {
+		std::string version_string;
+		uint8_t minor_version;
+		uint8_t major_version;
+		uint8_t physical_layout;
+		uint8_t visual_layout;
+		uint8_t _unknown3;
+		uint8_t _unknown4;
+		std::optional<uint8_t> _unknown5;
+	};
+
+	template <class T>
+	using Error = std::expected<T, std::string>;
+
 	AimoKeyboardDriver(){};
 	AimoKeyboardDriver(std::string name, std::vector<hid_device *> hiddev, uint16_t pid);
 	~AimoKeyboardDriver();
 
-	AimoKeyboardConfig config;
+	Error<DeviceInfo> get_device_info ();
+
+	Config config;
 	hid_device *ctrl_device;
 	hid_device *event_device;
 	hid_device *led_device;
@@ -34,7 +68,9 @@ class AimoKeyboardDriver {
   private:
 	ReadCallback *cb;
 	std::vector<uint8_t> await_response(uint8_t *command, uint8_t command_length);
+	bool check_checksum(uint8_t* buf, int size, uint8_t checksum_size);
 };
 
-static std::map<uint16_t, AimoKeyboardConfig> aimo_keyboard_config = {
+static std::map<uint16_t, AimoKeyboardDriver::Config> aimo_keyboard_config = {
+	{ROCCAT_VULCAN_100_AIMO_PID, {1}},
 	{ROCCAT_VULCAN_TKL_PRO_PID, {2}}};
