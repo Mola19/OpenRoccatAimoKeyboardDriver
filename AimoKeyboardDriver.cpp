c308168537ab6a4c171b4b1f1741cb20797c3613
Mola19 <Mola9041@gmail.com>
2025-10-26 03:08:00 +0100

    added getDeviceInfo for TKL Pro and 100

--- AimoKeyboardDriver.cpp
+++ AimoKeyboardDriver.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstring>
+#include <expected>
 #include <iostream>
 #include <string>
 #include <thread>
@@ -28,4 +29,67 @@ AimoKeyboardDriver::~AimoKeyboardDriver() {
 	hid_close(ctrl_device);
 	hid_close(event_device);
 	hid_close(led_device);
+}
+
+std::expected<AimoKeyboardDriver::DeviceInfo, std::string> AimoKeyboardDriver::get_device_info() {
+	uint8_t packet_length = (config.protocol_version == 1) ? 8 : 9;
+	uint8_t report_id = (config.protocol_version == 1) ? 0x0F : 0x09;
+
+	uint8_t* buf = new uint8_t[packet_length];
+	memset(buf, 0x00, packet_length);
+
+	buf[0] = report_id;
+	int read = hid_get_feature_report(ctrl_device, buf, packet_length);
+
+	if (read == -1)
+		return std::unexpected("HIDAPI Error");
+	
+	if (buf[0] != report_id || buf[1] != packet_length)
+		return std::unexpected("packet header is malformed");
+
+	if (config.protocol_version == 1 && !check_checksum(buf, packet_length, 1))
+		return std::unexpected("checksum didn't match");
+
+	char version[5];
+	snprintf(version, 5, "%d.%02d", buf[2] / 100, buf[2] % 100);
+
+	if (config.protocol_version == 1) {
+		return AimoKeyboardDriver::DeviceInfo{
+			.version_string = version,
+			.minor_version = static_cast<uint8_t>(buf[2] % 100),
+			.major_version = static_cast<uint8_t>(buf[2] / 100),
+			.physical_layout = buf[5],
+			.visual_layout = buf[6],
+			._unknown3 = buf[3],
+			._unknown4 = buf[4],
+		};
+	} else {
+		return AimoKeyboardDriver::DeviceInfo{
+			.version_string = version,
+			.minor_version = static_cast<uint8_t>(buf[2] % 100),
+			.major_version = static_cast<uint8_t>(buf[2] / 100),
+			.physical_layout = buf[6],
+			.visual_layout = buf[7],
+			._unknown3 = buf[3],
+			._unknown4 = buf[4],
+			._unknown5 = buf[5]};
+	}
+}
+
+bool AimoKeyboardDriver::check_checksum(uint8_t* buf, int size, uint8_t checksum_size) {
+	int sum = 0;
+
+	for (int i = 0; i < size - checksum_size; i++) {
+		sum += buf[i];
+	}
+
+	sum = sum % (1 << (checksum_size * 8));
+
+	int checksum = 0;
+
+	for (int i = size - checksum_size; i < size; i++) {
+		checksum += buf[i] * (1 << ((size - i - 1) * 8));
+	}
+
+	return checksum == sum;
 }
